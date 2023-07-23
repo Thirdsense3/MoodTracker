76f1eac892e287ddf0762fd37fdc21b027234fe0
Thirdsense <myu1231@naver.com>
2023-07-23 23:17:20 +0900

    feat : Feeling Service API 응답 부분 추가

--- src/main/java/com/myung/MoodTracker/service/FeelingServiceImpl.java
+++ src/main/java/com/myung/MoodTracker/service/FeelingServiceImpl.java
@@ -1,4 +1,14 @@
 package com.myung.MoodTracker.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeelingServiceImpl implements FeelingService{
+    @Override
+    public void recommendSongByFeeling(String feeling) {
+        System.out.println("feeling = " + feeling);
+
+
+
+    }
 }
